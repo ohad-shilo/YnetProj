5af4e4a6b9c932046fc397ac322e6381f8578816
ohad-shilo <50577604+ohad-shilo@users.noreply.github.com>
2020-12-29 12:03:12 +0200

    Removed code not in used

--- src/main/java/Infra/CommomConstants.java
+++ src/main/java/Infra/CommomConstants.java
@@ -3,30 +3,6 @@ package Infra;
 import com.sun.codemodel.internal.fmt.JStaticFile;
 
 public interface CommomConstants {
-    public static final String URL_CONTRIES = "https://af-qa-test.vercel.app/countries";
-    public static final String URL_HOME = "https://af-qa-test.vercel.app";
-
-    public static final String HOME_PAGE_TITLE = "React App";
-    public static final String COUNTRIES_PAGE_TITLE = "React App";
-
-    public static final String[] COUNTRIES_LIST = {"Armenia selector", "Antarctica selector", "Aruba selector"};
-
-    public static final String GO_TO_COUNTRIES_XPATH  = "//*[@id='root']/div/div[3]/table/tbody/tr[5]/td[4]";
-
-    public static final String OUTPUT_FOLDER = ".//TestRunResults";
-
-    public static final String JSON_FILE_NAME = "json_report.json";
-    public static final String SCREEN_SHOT_FILE_NAME = "error_screenshoot.png";
-    public static final String LOG_REPORT_FILE_NAME = "log_report.log";
-
-    public static final String REPORT_FILE_NAME = "Run_report.html";
-    public static final String REPORT_PAGE_TITLE = "Report Page Title";
-    public static final String REPORT_NAME = "The Report Name";
-
-
-    public static final String LINKEDIN_INVITED ="https://www.linkedin.com/groups/13760531/manage/membership/invited/";
-    public static final String LINKEDIN_NETWORK ="https://www.linkedin.com/mynetwork/";
-
 
 
     public static final String URL_GOOGLE = "https://www.google.com";
