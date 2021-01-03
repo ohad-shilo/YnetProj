c32d6095298e114fdfb9d923cd3a5666ac1ae8de
ohad-shilo <50577604+ohad-shilo@users.noreply.github.com>
2021-01-03 07:34:10 +0200

    Update CommomConstants.java

--- src/main/java/Infra/CommomConstants.java
+++ src/main/java/Infra/CommomConstants.java
@@ -7,6 +7,7 @@ public interface CommomConstants {
 
     public static final String URL_GOOGLE = "https://www.google.com";
     public static final String URL_YNET_NEWS = "https://www.ynetnews.com/category/3083";
+    public static final String URL_YNET_MAGAZINE = "https://www.ynetnews.com/magazine";
     public static final String HOME_PAGE_TITLE_YNET = "ynetnews - Homepage";
     public static final String GOOGLE_SEARCH_XPATH = "//input[@name='q']";
     public static final String YNET_NEWS_XPATH = "//*[@id='rso']/div[1]/div/div[1]/a/h3/span";
